985b2cf96f23e94a52c6ac00b7e8b36fa25de03e
divyaloganathan <LDivya@users.noreply.github.com>
2017-02-02 11:54:33 +0530

    Add files via upload

---
+++ ideone_UPh0MH.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int num;
+	scanf("%d",&num);
+	if(num>0)
+	printf("positive");
+	else if(num<0)
+	printf("negative");
+	else
+	printf("zero");
+	return 0;
+}
+
+	return 0;
+}
